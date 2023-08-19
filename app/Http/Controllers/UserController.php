f274058b194a87a50f5d9a373c9d60f535e92a7b
junaidnasir1001 <junaidnasir1001@gmail.com>
2023-08-19 18:21:20 +0500

    some error resolve

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -14,7 +14,6 @@ class UserController extends Controller
     public function index()
     {
         $users =  User::all();
-        //dd($user);
         return view('user.index', compact('users'));
     }
 
